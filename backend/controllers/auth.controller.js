c2414430e62632ecf9e41122fe318b4b2090b7ff
Shriharsh <shriharshgautam7@gmail.com>
2024-08-07 16:56:16 +0530

    created signup function

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,7 +1,61 @@
+ import User from '..models/user.models.js'
+ import bcrypt from 'bcryptjs'
+ 
  export const signup = async (req,res) => {
-    res.json({
-        data: "You have hit the signup Endpoint",
-    })
+   try {
+    const {fullName, username, password, email } = req.body;
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+        return res.status(400).json({ error: "Invalid email format"});
+    }
+     const existingUser = await User.findOne({username});
+     if (existingUser) {
+        return res.status(400).json({error: 'Username already exists'});
+     }
+     
+     const existingEmail =  await User.findOne({email});
+     if (existingEmail) {
+        return res.status(400).json({error: "Email already exists"});
+     }
+
+     //hashing the password
+     const salt = await bcrypt.genSalt(10);
+     const hashedPassword = await bcrypt.hash(password, salt);
+
+     const newUser = new User({
+        fullName,
+        username,
+        email,
+        password: hashedPassword
+     })
+
+     if (newUser) {
+        generateTokenAndSetCookie(newUser._id,res);
+        await newUser.save();
+
+        res.status(201).json({
+            _id: newUser._id,
+            fullName: newUser.fullName,
+            username: newUser.username,
+            email: newUser.email,
+            followers: newUser.followers,
+            following: newUser.following,
+            profileImg: newUser.profileImg,
+            coverImg: newUser.coverImg,
+
+        })
+
+     }
+
+     else {
+        res.status(400).json({ error: 'Invalid user data'});
+     }
+
+   } catch (error) {
+    res.status(500).json({error : 'Internal server error'});
+
+   }
 }
 
 export const login = async (req,res) => {
