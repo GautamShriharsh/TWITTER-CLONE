52517914e426309450478b2768e4477b16352e7d
Shriharsh <shriharshgautam7@gmail.com>
2024-11-12 02:37:35 +0530

    created likeUnlikePost controller

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -1,3 +1,4 @@
+import Notification from "../models/notification.model.js";
 import User from "../models/user.model.js";
 import Post from "../models/post.model.js";
 import {v2 as cloudinary} from "cloudinary";
@@ -82,10 +83,50 @@ export const commentOnPost = async (req,res) => {
         post.comments.push(comment);
         await post.save();
 
-        res.status(200).json(post)     
+        res.status(200).json(post);
         
     } catch (error) {
         console.log("Error in commentOnPost controller: ", error);
+        res.status(500).json({error: "Internal server error"});
+    }
+}
+
+export const likeUnlikePost = async (req,res) => {
+    try {
+        const {id:postId} = req.params;
+        const userId = req.user._id;
+
+        const post = await Post.findById(postId);
+
+        if (!post) {
+            return res.status(404).json({error: "Post not found"});
+        }
+
+        const userLikedPost = post.likes.includes(userId)
+
+        if (userLikedPost) {
+            // Unlike the post
+            await Post.updateOne({_id:postId},{$pull: {likes: userId}})
+            res.status(200).json({message: "Post unliked successfully"})
+        }
+        else{
+            //like post
+            post.likes.push(userId);
+            await post.save();
+
+            const notification = new Notification({
+                from: userId,
+                to: post.user,
+                type: "like",
+         });
+         await notification.save();
+
+          res.status(200).json({message: "Post liked successfully"});
+        };
+
+
+    } catch (error) {
+        console.log({message: "Error in likeUnlikePost controller"});
         res.status(500).json({error: "Internal server error"});
     }
 }
@@ -95,5 +136,6 @@ export default {
     createPost,
     deletePost,
     commentOnPost,
+    likeUnlikePost,
   };
   
