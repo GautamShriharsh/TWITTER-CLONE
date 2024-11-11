30c15423752f5ecc266df9bf8d803e240459cee8
Shriharsh <shriharshgautam7@gmail.com>
2024-11-12 00:46:48 +0530

    created commentOnPost controller

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -25,7 +25,7 @@ export const createPost = async (req,res) => {
         const newPost = new Post({
             user: userId,
             text,
-            img
+            img,
         })
 
         await newPost.save();
@@ -63,9 +63,37 @@ export const deletePost = async (req,res) => {
     }
 }
 
+export const commentOnPost = async (req,res) => {
+    try {
+        const { text } = req.body;
+        const postId = req.params.id;
+        const userId = req.user._id;
+
+        if (!text) {
+            return res.status(400).json({error: "Text field is required"});
+        }
+        const post = await Post.findById(postId);
+
+        if (!post) {
+            return res.status(404).json({error: "Post not found"});
+        }
+        const comment = {user: userId, text}
+        
+        post.comments.push(comment);
+        await post.save();
+
+        res.status(200).json(post)     
+        
+    } catch (error) {
+        console.log("Error in commentOnPost controller: ", error);
+        res.status(500).json({error: "Internal server error"});
+    }
+}
+
 // Change this if necessary
 export default {
     createPost,
     deletePost,
+    commentOnPost,
   };
   
