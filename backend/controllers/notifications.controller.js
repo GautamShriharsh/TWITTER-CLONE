ab8249d7fc150b5905851cfab8e2b610e9610699
Shriharsh <shriharshgautam7@gmail.com>
2024-11-29 12:34:19 +0530

    created deleteNotification

--- backend/controllers/notifications.controller.js
+++ backend/controllers/notifications.controller.js
@@ -37,9 +37,22 @@ try {
 export const deleteNotification = async (req,res) => {
     try {
         const userId = req.user._id;
+        const notificationId = req.params.id;
 
-        await Notification.delete
+        const notification = await Notification.findbyId(notificationId);
+
+        if (!notification) {
+            return res.status(404).json({error: "Notification not found"});
+        }
+
+        if(notification.to.toString() !==  userId.toString()){
+            return res.status(403).json({error: "You are not allowed to delete this notification"});
+        }
+        await Notification.findByIdAndDelete(notificationId);
+        res.status(200).json({message: "Notification deleted successfully"});
+
     } catch (error) {
-        
+        console.log("Error in deleteNotification function", error.message);
+        res.status(500).json({error: "Internal Server Error"});
     }
 }
