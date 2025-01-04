cd81c72a055bbe10cf2b30689aa2d1e28ee9382b
Shriharsh <shriharshgautam7@gmail.com>
2025-01-04 15:40:23 +0530

    prepared app for deployment.

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from 'dotenv';
 import cookieParser from "cookie-parser";
 import {v2 as cloudinary} from "cloudinary";
-
+import path from "path";
 import authRoutes from './routes/auth.route.js';
 import userRoutes from './routes/user.route.js';
 import postRoutes from './routes/post.route.js';
@@ -23,6 +23,7 @@ cloudinary.config({
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 app.use(express.json({limit:"5mb"})); // to parse req.body
 //limit shouldn't be too high to prevent attacks from hacker
@@ -33,13 +34,21 @@ app.use(cookieParser());
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/posts', postRoutes);
-app.use("/api/notifications",notificationRoutes)
+app.use("/api/notifications",notificationRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+    
+    app.get("*", (req,res) => {
+        res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"));
+    });
+};
 
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
     connectMondoDB();
-})
+});
 
 
 
